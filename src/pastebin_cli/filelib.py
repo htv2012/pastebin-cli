c1fd442b04fcaf0dde4ee5a86f24f2ea9b816253
Hai Vu <haivu2004@gmail.com>
2025-02-06 12:01:03 -0800

    Add filelib.py

---
+++ src/pastebin_cli/filelib.py
@@ -0,0 +1,20 @@
+import click
+def get_content(filename: str):
+    """
+    Get the content of a file
+
+    If `filename` is "-", get content from stdin.
+
+    If `Filename` is None, open an editor so the user can create
+    the content.
+
+    Return the content of the file, or editor. In case of editor,
+    a None return value means the user did not create the content.
+    """
+    if filename is None:
+        content = click.edit("(Edit the content of the paste here)")
+    else:
+        with click.open_file(filename) as stream:
+            content = stream.read()
+
+    return content
